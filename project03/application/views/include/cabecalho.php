88483b448952c44fb689fac2127f8ad166842b3d
LEANDRO DA SILVA <32754449+leandro47@users.noreply.github.com>
2020-09-07 12:33:35 -0300

    work is done

--- project03/application/views/include/cabecalho.php
+++ project03/application/views/include/cabecalho.php
@@ -1,3 +1,6 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,4 +13,6 @@
 </head>
 
 <body>
- 
+ <div class="container text-center">
+     <img src="<?php echo base_url('assets/images/banner_topo.jpg') ?>" alt="" srcset="">
+ </div>
--- project03/application/views/include/rodape.php
+++ project03/application/views/include/rodape.php
@@ -1,6 +1,12 @@
-<hr>
-<h3>rodape</h3>
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<div class="container text-center">
+    <hr>
+    <p><?php echo date('yy') ?> &copy; Todos os direitos Reservados</p>
+</div>
 
 <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
 </body>
+
 </html>
